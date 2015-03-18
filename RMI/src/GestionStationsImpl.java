ce688f5ace04f8a6024a74209bd4af3356513bae
TheoChevalier <theo.chevalier11@gmail.com>
2015-03-18 14:35:08 +0100

    Création table clients

--- RMI/src/GestionStationsImpl.java
+++ RMI/src/GestionStationsImpl.java
@@ -1,26 +1,65 @@
+
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Random;
 
 
 public class GestionStationsImpl extends UnicastRemoteObject implements GestionStation {
+	static Connection conn;
 
-
-	protected GestionStationsImpl() throws RemoteException {
-		// TODO Auto-generated constructor stub
-		System.out.println("coucou");
-		
+	protected GestionStationsImpl(String nomBD) throws RemoteException {
+		try {
+		    Class.forName("org.h2.Driver");
+		    conn = DriverManager.getConnection("jdbc:h2:"+nomBD+";IGNORECASE=TRUE", "sa", "");
+		    // on cree un objet Statement qui va permettre l'execution des requetes
+	        Statement s = conn.createStatement();
+	
+	        // On regarde si la table existe deja
+	        String query = "select numC from CLIENTS limit 1";
+	        try {
+	        	s.executeQuery(query);
+	        } catch(Exception e) {
+	        	// sinon on l'a cree
+	        	s.execute("create table CLIENTS  ( " +
+	        			" numC VARCHAR( 256 ) NOT NULL PRIMARY KEY, " +
+	        			" nomC VARCHAR( 256 ) , " +
+	        			" mdpC VARCHAR( 10 ))");
+	        	// on ajoute des entrees de test
+	        	s.executeUpdate("insert into CLIENTS values ('5', 'Léa', '000000001')");
+	        	s.executeUpdate("insert into CLIENTS values ('6', 'Paul', '000000002')");
+	        }
+		} catch(Exception e) {
+			// il y a eu une erreur
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) throws RemoteException, MalformedURLException {
 		System.out.println("coucou2");
 		LocateRegistry.createRegistry(1099);
-		GestionStationsImpl gest = new GestionStationsImpl();
+		GestionStationsImpl gest = new GestionStationsImpl("ServiceVelos");
 		Naming.rebind("Gestionnaire", gest);
 		System.out.println("Running on port 1099");
+		
+		try {
+			Statement s = conn.createStatement();
+			ResultSet rs = s.executeQuery("select nomC from CLIENTS where numC = '5'");
+	        if (rs.next()) {
+	        	String nom = rs.getString("nomC");
+	        	System.out.println(nom);
+	        }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public int getClientMotDePasse() {
