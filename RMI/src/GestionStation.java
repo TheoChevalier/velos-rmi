c7b527bba8bb26a1f34600f5c4581e07db841995
Léa <La@130.120.210.211>
2015-03-20 16:30:34 +0100

    majCache

--- RMI/src/GestionStation.java
+++ RMI/src/GestionStation.java
@@ -4,6 +4,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.util.Random;
+import java.util.Vector;
 
 
 public interface GestionStation extends Remote {
@@ -14,4 +15,6 @@ public interface GestionStation extends Remote {
 	public void affecterVeloStation(String numVelo, String numStation) throws RemoteException;
 	public boolean rechercherVelo(String numVelo) throws RemoteException;
 	public boolean rechercherStation(String numStation) throws RemoteException;
+	public Vector majCacheStation(String numStation) throws RemoteException;
+	public Client rechercherClient(String numClient) throws RemoteException;
 }
